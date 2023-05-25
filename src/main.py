37b14afd3859e521b351800a6b0405acfc746582
Ali Toosi <alirezakhoshghalb@ymail.com>
2023-05-25 21:45:07 +1000

    Add initialization cycle to the client

--- src/main.py
+++ src/main.py
@@ -6,21 +6,33 @@ import comms
 import random
 import math
 
-message = comms.read_message()
 
-while message:
-    random_number = random.randint(1, 3)
-    if random_number == 1:
-        comms.post_message({
-            "path": [random.randint(100, 250), random.randint(100, 250)]
-        })
-    elif random_number == 2:
-        comms.post_message({
-            "shoot": random.uniform(0, math.pi*2)
-        })
-    else:
-        comms.post_message({
-            "path": [0, 0]
-        })
-
+def initialize_game():
     message = comms.read_message()
+    while message != "END_INIT":
+        message = comms.read_message()
+
+
+def play_game():
+    message = comms.read_message()
+    while message:
+        random_number = random.randint(1, 3)
+        if random_number == 1:
+            comms.post_message({
+                "path": [random.randint(100, 250), random.randint(100, 250)]
+            })
+        elif random_number == 2:
+            comms.post_message({
+                "shoot": random.uniform(0, math.pi*2)
+            })
+        else:
+            comms.post_message({
+                "path": [0, 0]
+            })
+
+        message = comms.read_message()
+
+
+if __name__ == "__main__":
+    initialize_game()
+    play_game()
